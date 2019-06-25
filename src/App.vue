6c25857379bd49a14a7e17ab8a5e621896d348f0
Dimiaun <damian.salguero@aikq.de>
2019-06-25 11:08:06 +0200

    Update Damian

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,8 @@
   <div id="app">
     <div class="nav-wrapper">
       <div id="nav">
-<<<<<<< HEAD
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-=======
         <router-link class="nav" to="/">Welcome</router-link> |
         <router-link class="nav" to="/about">About</router-link>
->>>>>>> f5ca1fe17d3f0d168cba9561845d6c10e441ccc5
       </div>
     </div>
     <router-view />
