279f2a14762b7938ffb1e64d7b1dea7eccc003ba
Dimiaun <damian.salguero@aikq.de>
2019-06-25 11:30:07 +0200

    Update master

--- src/router.js
+++ src/router.js
@@ -19,13 +19,13 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-
-    {
-      path: "/",
-      name: "landing",
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Landing.vue")
     }
+
+    // {
+    //   path: "/",
+    //   name: "landing",
+    //   component: () =>
+    //     import(/* webpackChunkName: "about" */ "./views/Landing.vue")
+    // }
   ]
 });
