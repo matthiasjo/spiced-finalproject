3b81797aa6253865d0b62e7820b04aabb079e5a5
Dimiaun <damian.salguero@aikq.de>
2019-06-25 10:37:41 +0200

    Add Landing to Damian

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="main">
     <div class="header">
-<<<<<<< HEAD
       <h1>DOG RESCUE</h1>
-=======
         <div class="title">
             <b-img
             id="header-logo"
@@ -19,21 +17,12 @@
             alt="Responsive image"
             ></b-img>
         </div>
->>>>>>> f5ca1fe17d3f0d168cba9561845d6c10e441ccc5
       <div class="subtitle">
         <h3>Welcome to our Dog Rescue Site</h3>
         <h3>Together we save lives</h3>
       </div>
-<<<<<<< HEAD
-      <b-img
-        id="header-logo"
-        src="https://www.onlygfx.com/wp-content/uploads/2018/01/grunge-paw-print-2.png"
-        fluid
-        alt="Responsive image"
-      ></b-img>
-=======
+
       
->>>>>>> f5ca1fe17d3f0d168cba9561845d6c10e441ccc5
     </div>
     <div class="landing-help">
       <h1>Join the team and <a href="#"> help us </a>out</h1>
@@ -41,8 +30,5 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-<style src="../../public/style.test.css" scoped></style>
-=======
+
 <style src="../../public/style.landing.css" scoped></style>
->>>>>>> f5ca1fe17d3f0d168cba9561845d6c10e441ccc5
