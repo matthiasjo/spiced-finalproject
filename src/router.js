253c4dcec37dceb2a3a5c7cf059a4f8477db1c3c
Matthias <18295017+matthiasjo@users.noreply.github.com>
2019-06-25 14:09:26 +0200

    packagecheck

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+//import Home from "./views/Home.vue";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
@@ -19,8 +19,6 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
-<<<<<<< HEAD
-=======
     },
     {
       path: "/auth",
@@ -32,7 +30,6 @@ export default new Router({
       name: "ResetPass",
       component: () =>
         import(/* webpackChunkName: "resetPass" */ "./components/auth/ResetPass.vue")
->>>>>>> beta
     }
 
     // {
