15a6f01e8f6c2d5cb25ee0ed51c0c0ea378094f0
Dimiaun <damian.salguero@aikq.de>
2019-06-26 15:25:30 +0200

    Update Damian

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         <router-link class="nav" to="/">Welcome</router-link> |
         <router-link class="nav" to="/about">About</router-link> |
         <router-link class="nav" to="/events">Events</router-link> |
-        <router-link class="nav" to="/adobtion">Adoption</router-link> |
+        <router-link class="nav" to="/adoption">Adoption</router-link> |
         <router-link v-if="userInfo.userId == null" class="nav" to="/auth"
           >Authentication</router-link
         >
